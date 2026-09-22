107c7efe38973c8b3d02f23d7e8acd0e7cd03c2c
apsevcenco <shapp.aps@gmail.com>
2026-09-22 12:13:15 +0200

    Declare backend URL global

--- frontend/src/globals.d.ts
+++ frontend/src/globals.d.ts
@@ -4,6 +4,7 @@ declare global {
   interface Window {
     ST_MICHAEL_SUPABASE_URL?: string;
     ST_MICHAEL_SUPABASE_ANON_KEY?: string;
+    ST_MICHAEL_BACKEND_URL?: string;
     supabase?: {
       createClient: (url: string, key: string) => any;
     };
